73ba98c53b1a5306d682fc27d88f9ddb92c72eef
allan2 <allanzhang7@gmail.com>
2022-02-23 00:51:47 -0330

    Fix column name error in train

--- 5_classifying.R
+++ 5_classifying.R
@@ -12,6 +12,9 @@ df <- read.table("data/SeqTab_NoChim_SamplesInColumns_in24Dogs.tsv",
 )
 # Transpose so that OTUs are columns. The index is "SampleN".
 df <- as.data.frame(t(df))
+# We don't need the full sequence as the column names. Let's replace them with numbers.
+colnames(df) <- seq_len(length(colnames(df)))
+
 
 # The index is "SampleN".
 sample_vars <- read.csv("data/samplevariables.csv",
@@ -32,16 +35,13 @@ df$anxiety <- factor(df$anxiety)
 df$aggression <- factor(df$aggression)
 
 if (sample_mode) {
-    df <- df[sample(nrow(df), 100), ]
+    df <- df[sample(nrow(df), 20), ]
 }
 
 set.seed(1251)
-# Use the entire data set because we have few dogs.
-m <- sqrt(ncol(df))
-tune_grid <- expand.grid(.mtry = c(sqrt(ncol(df))))
 
-# Train a random forest for a given classifier (colname).
-train <- function(colname) {
+# Train a random forest for a given classifier.
+train_anxiety <- function(n_tree, tune_grid) {
     # 10-fold repeating three times
     ctrl <- caret::trainControl(
         method = "repeatedcv",
@@ -51,40 +51,46 @@ train <- function(colname) {
         savePredictions = TRUE # for ROC plotting
     )
 
-    # Tune manually by varying the number of trees.
-    n_trees <- seq(100, 500, 100)
-    models <- list()
     # Train.
-    for (n_tree in n_trees) {
-        start_time <- Sys.time()
-        fit <- caret::train(
-            colname ~ .,
-            data = df,
-            method = "rf",
-            tuneGrid = tune_grid,
-            trControl = ctrl,
-            metric = "Accuracy",
-            ntree = n_tree
-        )
-        end_time <- Sys.time()
-        elapsed <- end_time - start_time
-        print(elapsed)
-        models[[toString(n_tree)]] <- fit
-    }
-
-    results <- caret::resamples(models)
-    summary(results)
-    png("output/5_dotplot_accuracy.png")
-    lattice::dotplot(results)
-    dev.off()
-
-    # TODO: Only labels with 2 classes supported
-    ggplot(fit$pred, aes(
-        m = fit$pred$M,
-        d = fit$pred$obs
-    )) +
-        geom_roc(hjust = -0.4, vjust = 1.5) +
-        coord_equal()
+    start_time <- Sys.time()
+    fit <- caret::train(
+        anxiety ~ .,
+        data = df,
+        method = "rf",
+        tuneGrid = tune_grid,
+        trControl = ctrl,
+        metric = "Accuracy",
+        ntree = n_tree
+    )
+    end_time <- Sys.time()
+    elapsed <- end_time - start_time
+    print(elapsed)
+    fit
 }
 
-train("anxiety")
+
+n_trees <- seq(100, 500, 100)
+m_values <- c(log(ncol(df)), sqrt(ncol(df)), ncol(df) / 4)
+
+# Store results
+models <- list()
+for (m in m_values) {
+    # Use the entire data set because we have few dogs.
+    tune_grid <- expand.grid(.mtry = c(sqrt(ncol(df))))
+    for (n_tree in n_trees) {
+        fit <- train_anxiety(n_tree, tune_grid)
+        models[[toString(n_tree)]] <- fit
+    }
+}
+results <- caret::resamples(models)
+summary(results)
+png("output/5_dotplot_accuracy_anxiety.png")
+lattice::dotplot(results)
+dev.off()
+# TODO: Only labels with 2 classes supported
+ggplot(fit$pred, aes(
+    m = fit$pred$M,
+    d = fit$pred$obs
+)) +
+    geom_roc(hjust = -0.4, vjust = 1.5) +
+    coord_equal()
