b031894e7eb97901b00a29e4a26b9ff7667da6a6
allan2 <allanzhang7@gmail.com>
2022-03-07 19:21:19 -0330

    Redo data prep

--- 5_classifying.R
+++ 5_classifying.R
@@ -1,14 +1,21 @@
 source("find_hyperparams.R")
 
-# The sample dataset
 df <- prep()
-# df <- df[sample(nrow(df), 200), ]
+
 # Remove the aggression column from the anxiety table.
 df_anx <- subset(df, select = -c(aggression))
 # Remove the anxiety column from the aggression table.
 df_aggr <- subset(df, select = -c(anxiety))
+
 ctrl <- train_ctrl()
 
 set.seed(1)
 run("anx", df_anx, ctrl, train_anxiety)
-# run("aggr", df_aggr, ctrl, train_aggression)
+run("aggr", df_aggr, ctrl, train_aggression)
+
+# Only keep our six genuses/families.
+df_fam <- prep(select_families = TRUE)
+df_anx_fam <- subset(df_fam, select = -c(aggression))
+df_aggr_fam <- subset(df_fam, select = -c(anxiety))
+run("anx_fam", df_anx_fam, ctrl, train_anxiety)
+run("aggr_fam", df_aggr_fam, ctrl, train_aggression)
--- find_hyperparams.R
+++ find_hyperparams.R
@@ -1,37 +1,34 @@
 library(caret)
 
-seq_taxa <- function() {
-    df <- read.table("data/SeqTab_NoChim_SamplesInColumns_Taxa.tsv",
-        sep = "\t", header = TRUE, row.names = 1
-    )
-
-    # Define taxanomic columns.
-    tx_cols <- c(
-        "seq",
-        "Kingdom",
-        "Phylum",
-        "Class",
-        "Order",
-        "Family",
-        "Genus"
-    )
-    # Only keep taxa columns.
-    df[, colnames(df) %in% tx_cols]
-}
-
 # Reads the data into the form we want.
-prep <- function() {
+prep <- function(select_families = FALSE) {
     # Read count data.
     df <- read.table("data/SeqTab_NoChim_SamplesInColumns_in24Dogs.tsv",
         sep = "\t", header = TRUE, row.names = 1
     )
-    # num_rows <- nrow(df)
-    # taxa <- seq_taxa()
-    # # Inner join on the sequence ID (the index).
-    # df <- merge(df, taxa, by = 0)
-    # if (num_rows != nrow(df)) {
-    #     stop("Inner join with the taxa table reduced the number of rows")
-    # }
+
+    if (select_families) {
+        taxa <- read.table("data/SeqTab_NoChim_SamplesInColumns_Taxa.tsv",
+            sep = "\t", header = TRUE, row.names = 1
+        )
+        # Only keep taxa columns.
+        taxa <- taxa[, colnames(taxa) %in% c("Family", "Genus")]
+
+        family_keep <- c(
+            "Bacteroidaceae",
+            "Oscillospiraceae", "Ruminococcaceae"
+        )
+        genus_keep <- c("Blautia", "Faecalibacterium", "Bacteroides")
+        # Filter taxa to keep only the families and genuses we want.
+        taxa <- taxa[
+            (taxa$Family %in% family_keep |
+                taxa$Genus %in% genus_keep),
+        ]
+        # Inner join on the sequence ID (the index).
+        df <- merge(df, taxa, by = 0)
+        # Remove taxa columns and the "Row.name" column that was created after merge.
+        df <- subset(df, select = -c(Row.names, Family, Genus))
+    }
 
     # Transpose so that OTUs are columns. The index is "SampleN".
     df <- as.data.frame(t(df))
@@ -77,7 +74,6 @@ train_ctrl <- function() {
 }
 
 train_anxiety <- function(df, ctrl, n_tree, tune_grid) {
-    print(nrow(df))
     caret::train(
         anxiety ~ .,
         data = df,
@@ -91,7 +87,6 @@ train_anxiety <- function(df, ctrl, n_tree, tune_grid) {
 }
 
 train_aggression <- function(df, ctrl, n_tree, tune_grid) {
-    print(nrow(df))
     caret::train(
         aggression ~ .,
         data = df,
@@ -108,34 +103,46 @@ output_filename <- function(label, idx) {
     paste0(label, idx, ".csv")
 }
 
-
+# The function to run the training through different values of m and n_tree
+# Output is saved as a csv.
+#
+# label: label for the output file, e.g., "anx" or "aggr"
+# df: data frame with the samples as rows,
+#     the sequences as columns, and the classifier as a column
+# ctrl: output of train_ctrl()
+# train_fn: the training function, such as train_anxiety or train_aggression
 run <- function(label, df, ctrl, train_fn) {
+    print(paste0("Running ", label, "..."))
     n_trees <- seq(100, 500, 100)
     # Because we remove either accuracy or aggression column
     n_col <- ncol(df) - 1
     m_values <- c(log(n_col), sqrt(n_col), (n_col / 4))
     ctrl <- train_ctrl()
     idx <- 0
+    results <- data.frame()
     for (m in m_values) {
-        print(idx)
-        idx <- idx + 1
         # Use the entire data set because we have few dogs.
         tune_grid <- expand.grid(.mtry = m)
         for (n_tree in n_trees) {
+            idx <- idx + 1
             start_time <- Sys.time()
             fit <- train_fn(df, ctrl, n_tree, tune_grid)
             end_time <- Sys.time()
             elapsed <- end_time - start_time
-            print(elapsed)
+            print(paste0(
+                "Iteration ", idx, "\tm: ", round(m, 1),
+                "\tn_tree: ", n_tree, "\tElapsed: ", round(elapsed, 1), " s"
+            ))
             res <- data.frame(fit$results)
-            filename <- paste0(label, idx, ".csv")
-            write.table(res,
-                filename,
-                append = FALSE,
-                sep = ",",
-                row.names = FALSE,
-                col.names = TRUE
-            )
+            results <- rbind(results, res)
         }
     }
+    filename <- paste0(label, ".csv")
+    write.table(results,
+        filename,
+        append = FALSE,
+        sep = ",",
+        row.names = TRUE,
+        col.names = NA # leave a blank in the header for the data frame index
+    )
 }
